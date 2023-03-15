e9a9f384e07dabd2c60d8715c60b899cace3e6c6
Takeru Ohta <phjgt308@gmail.com>
2023-03-15 11:05:09 +0900

    wasm ビルドに対応

--- packages/light-adjustment/zig/build.zig
+++ packages/light-adjustment/zig/build.zig
@@ -8,6 +8,10 @@ pub fn build(b: *std.Build) void {
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
     // for restricting supported target set are available.
+
+    // 以下のオプションを指定すれば wasm 用にビルドできるが、ユニットテスト実行が失敗するので、
+    // 代わりに `$ zig build -Dtarget=wasm32-freestanding` のようにビルド時にコマンドライン引数を指定することで対処する
+    // const target = b.standardTargetOptions(.{ .default_target = .{ .cpu_arch = .wasm32, .os_tag = .freestanding } });
     const target = b.standardTargetOptions(.{});
 
     // Standard optimization options allow the person running `zig build` to select
@@ -15,7 +19,8 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
+    // wasm をビルドするためには addStaticLibrary() ではなく addSharedLibrary() を使う必要がある
+    const lib = b.addSharedLibrary(.{
         .name = "zig",
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
