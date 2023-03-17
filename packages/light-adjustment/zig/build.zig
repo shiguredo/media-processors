e5b7b416de78ff0e9c61abf050d91e4440ea0039
Takeru Ohta <phjgt308@gmail.com>
2023-03-17 14:31:36 +0900

    WebAssembly 関連処理を main.zig から分離して wasm.zig に集約

--- packages/light-adjustment/zig/build.zig
+++ packages/light-adjustment/zig/build.zig
@@ -24,7 +24,7 @@ pub fn build(b: *std.Build) void {
         .name = "light_adjustment",
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = .{ .path = "src/wasm.zig" },
         .target = target,
         .optimize = optimize,
     });
