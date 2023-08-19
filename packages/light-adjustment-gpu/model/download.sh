ecd3a9720b32c0d0e6a10fca4795733a42724a29
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2023-08-19 20:05:25 +0900

    使用モデル・コードの整理

--- packages/light-adjustment-gpu/model/download.sh
+++ packages/light-adjustment-gpu/model/download.sh
@@ -3,31 +3,13 @@
 set -e
 
 SG_LLIE_FILES="semantic_guided_llie_720x1284.onnx semantic_guided_llie_360x648.onnx semantic_guided_llie_480x648.onnx semantic_guided_llie_240x324.onnx"
-SCI_FILES="sci_difficult_240x320.onnx sci_difficult_360x640.onnx sci_difficult_480x640.onnx sci_difficult_720x1280.onnx sci_easy_240x320.onnx sci_easy_360x640.onnx sci_easy_480x640.onnx sci_easy_720x1280.onnx sci_medium_240x320.onnx sci_medium_360x640.onnx sci_medium_480x640.onnx sci_medium_720x1280.onnx"
-PMN_FILES="pmn_384x640.onnx pmn_480x640.onnx pmn_736x1280.onnx"
 
-MODELS="${SG_LLIE_FILES//.onnx/} ${SCI_FILES//.onnx/} ${PMN_FILES//.onnx/}"
+MODELS="${SG_LLIE_FILES//.onnx/}"
 
 for f in $SG_LLIE_FILES; do
   if [ ! -e $f ]; then
     curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/370_Semantic-Guided-Low-Light-Image-Enhancement/resources.tar.gz" -o resources.tar.gz
     tar -zxvf resources.tar.gz $SG_LLIE_FILES
-    rm resources.tar.gz
-  fi
-done
-
-for f in $SCI_FILES; do
-  if [ ! -e $f ]; then
-    curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/286_SCI/resources.tar.gz" -o resources.tar.gz
-    tar -zxvf resources.tar.gz $SCI_FILES
-    rm resources.tar.gz
-  fi
-done
-
-for f in $PMN_FILES; do
-  if [ ! -e $f ]; then
-    curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/349_PMN/resources.tar.gz" -o resources.tar.gz
-    tar -zxvf resources.tar.gz $PMN_FILES
     rm resources.tar.gz
   fi
 done
--- packages/light-adjustment-gpu/src/low_light_image_enhance.ts
+++ packages/light-adjustment-gpu/src/low_light_image_enhance.ts
@@ -6,10 +6,13 @@ interface ImageToImageModelOption {
   modelPath: string;
   inputWidth: number;
   inputHeight: number;
+  // LLIEではinputWidth/Height = outputWidth/Heightだが、超解像を使う場合には必要なので作っておく
   outputWidth: number;
   outputHeight: number;
+  // Semantic Guided Low-light Image Enhancementでは必要ないが、他のモデルを使う場合には必要なので作っておく
   inputConverter?: (tensor: tf.Tensor3D) => tf.Tensor<tf.Rank> | tf.NamedTensorMap | tf.Tensor<tf.Rank>[];
   outputConverter?: (tensor: tf.Tensor | tf.Tensor[] | tf.NamedTensorMap) => tf.Tensor3D;
+  // 超解像用
   outputSizeIsModelOutputSize?: boolean;
 }
 
@@ -111,13 +114,6 @@ abstract class ImageToImageVideoProcessor {
         outputResizeCanvas.width = image.width;
         outputResizeCanvas.height = image.height;
       }
-      // if (
-      //   outputResizeCanvas.width !== this.modelOption.outputWidth ||
-      //   outputResizeCanvas.height !== this.modelOption.outputHeight
-      // ) {
-      //   outputResizeCanvas.width = this.modelOption.outputWidth;
-      //   outputResizeCanvas.height = this.modelOption.outputHeight;
-      // }
       const inputConverter = this.modelOption.inputConverter ?? ((img) => tf.div(tf.expandDims(img), 255.0));
       const output = tf.tidy(() => {
         const img = tf.browser.fromPixels(inputResizeCanvas);
@@ -145,9 +141,6 @@ abstract class ImageToImageVideoProcessor {
         gl.bindFramebuffer(gl.READ_FRAMEBUFFER, frameBuffer);
         gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, null);
         gl.clearBufferfv(gl.COLOR, 0, [0.0, 0.0, 0.0, 1.0]);
-        // if (gl.checkFramebufferStatus(gl.READ_FRAMEBUFFER) !== gl.FRAMEBUFFER_COMPLETE) {
-        //   throw new Error(`invalid framebuffer status: ${gl.checkFramebufferStatus(gl.READ_FRAMEBUFFER)}`);
-        // }
         gl.blitFramebuffer(
           0,
           0,
@@ -196,36 +189,7 @@ export enum llieModelNames {
   semanticGuidedLlie648x480 = "semantic_guided_llie_648x480",
   semanticGuidedLlie648x360 = "semantic_guided_llie_648x360",
   semanticGuidedLlie324x240 = "semantic_guided_llie_324x240",
-  sciDifficult1280x720 = "sci_difficult_1280x720",
-  sciDifficult640x360 = "sci_difficult_640x360",
-  sciDifficult640x480 = "sci_difficult_640x480",
-  sciDifficult320x240 = "sci_difficult_320x240",
-  sciMedium1280x720 = "sci_medium_1280x720",
-  sciMedium640x360 = "sci_medium_640x360",
-  sciMedium640x480 = "sci_medium_640x480",
-  sciMedium320x240 = "sci_medium_320x240",
-  sciEasy1280x720 = "sci_easy_1280x720",
-  sciEasy640x360 = "sci_easy_640x360",
-  sciEasy640x480 = "sci_easy_640x480",
-  sciEasy320x240 = "sci_easy_320x240",
-  pmn1280x736 = "pmn_1280x736",
 }
-
-const outputConverter255 = (outputTensor: tf.Tensor | tf.Tensor[] | tf.NamedTensorMap): tf.Tensor3D => {
-  const outputRgb = tf.div(outputTensor as tf.Tensor4D, 255).squeeze();
-  const shape = outputRgb.shape as [number, number, number];
-  const outputA = tf.fill([shape[0], shape[1], 1], 1.0, "float32");
-  return tf.concat3d([outputRgb, outputA] as tf.Tensor3D[], 2);
-};
-
-const inputConverterPmn: (img: tf.Tensor3D) => tf.Tensor4D = (img) => {
-  const imgA = tf.fill([img.shape[0], img.shape[1], 1], 1.0, "float32") as tf.Tensor3D;
-  return tf.expandDims(tf.concat3d([tf.div(img, 255), imgA] as tf.Tensor3D[], 2));
-};
-
-const outputConverterPmn = (outputTensor: tf.Tensor | tf.Tensor[] | tf.NamedTensorMap): tf.Tensor3D => {
-  return (outputTensor as tf.Tensor4D).squeeze();
-};
 
 const llieModels: { [key: string]: ImageToImageModelOption } = {
   semantic_guided_llie_1284x720: {
@@ -256,156 +220,11 @@ const llieModels: { [key: string]: ImageToImageModelOption } = {
     outputWidth: 648,
     outputHeight: 360,
   },
-  sci_difficult_1280x720: {
-    modelPath: "tfjs_model_sci_difficult_720x1280",
-    inputWidth: 1280,
-    inputHeight: 720,
-    outputWidth: 1280,
-    outputHeight: 720,
-    outputConverter: outputConverter255,
-  },
-  sci_difficult_640x360: {
-    modelPath: "tfjs_model_sci_difficult_360x640",
-    inputWidth: 640,
-    inputHeight: 360,
-    outputWidth: 640,
-    outputHeight: 360,
-    outputConverter: outputConverter255,
-  },
-  sci_difficult_640x480: {
-    modelPath: "tfjs_model_sci_difficult_480x640",
-    inputWidth: 640,
-    inputHeight: 480,
-    outputWidth: 640,
-    outputHeight: 480,
-    outputConverter: outputConverter255,
-  },
-  sci_difficult_320x240: {
-    modelPath: "tfjs_model_sci_difficult_240x320",
-    inputWidth: 320,
-    inputHeight: 240,
-    outputWidth: 320,
-    outputHeight: 240,
-    outputConverter: outputConverter255,
-  },
-  sci_medium_1280x720: {
-    modelPath: "tfjs_model_sci_medium_720x1280",
-    inputWidth: 1280,
-    inputHeight: 720,
-    outputWidth: 1280,
-    outputHeight: 720,
-    outputConverter: outputConverter255,
-  },
-  sci_medium_640x360: {
-    modelPath: "tfjs_model_sci_medium_360x640",
-    inputWidth: 640,
-    inputHeight: 360,
-    outputWidth: 640,
-    outputHeight: 360,
-    outputConverter: outputConverter255,
-  },
-  sci_medium_640x480: {
-    modelPath: "tfjs_model_sci_medium_480x640",
-    inputWidth: 640,
-    inputHeight: 480,
-    outputWidth: 640,
-    outputHeight: 480,
-    outputConverter: outputConverter255,
-  },
-  sci_medium_320x240: {
-    modelPath: "tfjs_model_sci_medium_240x320",
-    inputWidth: 320,
-    inputHeight: 240,
-    outputWidth: 320,
-    outputHeight: 240,
-    outputConverter: outputConverter255,
-  },
-  sci_easy_1280x720: {
-    modelPath: "tfjs_model_sci_easy_720x1280",
-    inputWidth: 1280,
-    inputHeight: 720,
-    outputWidth: 1280,
-    outputHeight: 720,
-    outputConverter: outputConverter255,
-  },
-  sci_easy_640x360: {
-    modelPath: "tfjs_model_sci_easy_360x640",
-    inputWidth: 640,
-    inputHeight: 360,
-    outputWidth: 640,
-    outputHeight: 360,
-    outputConverter: outputConverter255,
-  },
-  sci_easy_640x480: {
-    modelPath: "tfjs_model_sci_easy_480x640",
-    inputWidth: 640,
-    inputHeight: 480,
-    outputWidth: 640,
-    outputHeight: 480,
-    outputConverter: outputConverter255,
-  },
-  sci_easy_320x240: {
-    modelPath: "tfjs_model_sci_easy_240x320",
-    inputWidth: 320,
-    inputHeight: 240,
-    outputWidth: 320,
-    outputHeight: 240,
-    outputConverter: outputConverter255,
-  },
-  pmn_1280x736: {
-    modelPath: "tfjs_model_pmn_736x1280",
-    inputWidth: 1280,
-    inputHeight: 736,
-    outputWidth: 1280,
-    outputHeight: 736,
-    inputConverter: inputConverterPmn,
-    outputConverter: outputConverterPmn,
-  },
-  pmn_640x384: {
-    modelPath: "tfjs_model_pmn_384x640",
-    inputWidth: 640,
-    inputHeight: 384,
-    outputWidth: 640,
-    outputHeight: 384,
-    inputConverter: inputConverterPmn,
-    outputConverter: outputConverterPmn,
-  },
 };
 
 export class LightAdjustmentGpuProcessor extends ImageToImageVideoProcessor {
   constructor(assetPath: string, modelName: llieModelNames, alpha: number) {
     const model = llieModels[modelName];
     super(Object.assign({}, model, { modelPath: assetPath + model.modelPath }), alpha);
-  }
-}
-
-export enum superResolutionModelNames {
-  rfdn320x180 = "rfdn_320x180",
-  fastSrgan180x120 = "fast_srgan_180x120",
-}
-
-const superResolutionModels: { [key: string]: ImageToImageModelOption } = {
-  rfdn_320x180: {
-    modelPath: "tfjs_model_rfdn_180x320",
-    inputWidth: 320,
-    inputHeight: 180,
-    outputWidth: 1280,
-    outputHeight: 720,
-    outputSizeIsModelOutputSize: true,
-  },
-  fast_srgan_180x120: {
-    modelPath: "tfjs_model_Fast-SRGAN-120x160",
-    inputWidth: 160,
-    inputHeight: 120,
-    outputWidth: 640,
-    outputHeight: 480,
-    outputSizeIsModelOutputSize: true,
-  },
-};
-
-export class SuperResolutionProcessor extends ImageToImageVideoProcessor {
-  constructor(assetPath: string, modelName: superResolutionModelNames) {
-    const model = superResolutionModels[modelName];
-    super(Object.assign({}, model, { modelPath: assetPath + model.modelPath }), 1.0);
   }
 }
