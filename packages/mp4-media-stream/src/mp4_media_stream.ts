066585e5123dc8a9d96a92a1be2378ee45bb8000
Takeru Ohta <phjgt308@gmail.com>
2024-11-25 12:25:05 +0900

    Update isSupported()

--- packages/mp4-media-stream/src/mp4_media_stream.ts
+++ packages/mp4-media-stream/src/mp4_media_stream.ts
@@ -50,7 +50,6 @@ class Mp4MediaStream {
    */
   static isSupported(): boolean {
     return !(
-      typeof MediaStreamTrackGenerator === 'undefined' ||
       typeof AudioDecoder === 'undefined' ||
       typeof VideoDecoder === 'undefined'
     )
