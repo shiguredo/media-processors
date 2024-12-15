08cb091e08bbbf5ea4d1cff47b4fdc4f426e13c4
voluntas <nakai@shiguredo.jp>
2024-12-15 15:23:38 +0900

    mp4-media-stream に vite.config.mjs を追加

---
+++ packages/mp4-media-stream/vite.config.mjs
@@ -0,0 +1,54 @@
+import fs from 'node:fs'
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import pkg from './package.json'
+
+const banner = `/**
+ * ${pkg.name}
+ * ${pkg.description}
+ * @version: ${pkg.version}
+ * @author: ${pkg.author}
+ * @license: ${pkg.license}
+ **/
+`
+
+export default defineConfig({
+  build: {
+    minify: 'esbuild',
+    target: 'es2023',
+    emptyOutDir: true,
+    manifest: true,
+    lib: {
+      entry: resolve(__dirname, 'src/mp4_media_stream.ts'),
+      formats: ['es'],
+      name: 'Shiguredo',
+      fileName: 'mp4_media_stream',
+    },
+    rollupOptions: {
+      output: {
+        banner: banner,
+      },
+      plugins: [
+        {
+          name: 'wasm-loader',
+          transform(code) {
+            return code.replace(
+              /__WASM__/g,
+              () => fs.readFileSync(
+                "../../target/wasm32-unknown-unknown/release/mp4_media_stream.wasm",
+                "base64"
+              )
+            )
+          }
+        }
+      ],
+    },
+  },
+  plugins: [
+    dts({
+      include: ['src/**/*'],
+    }),
+  ],
+})
