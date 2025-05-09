173d5f772165fdaa6d82e952a1adbdcac20423a6
voluntas <nakai@shiguredo.jp>
2025-05-09 13:02:55 +0900

    exports type main を追加

--- packages/noise-suppression/vite.config.mjs
+++ packages/noise-suppression/vite.config.mjs
@@ -1,8 +1,6 @@
-import fs from 'node:fs'
 import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 import pkg from './package.json'
 
 const banner = `/**
