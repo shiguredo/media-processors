21ebfc4ea5ab1dae762c4db1745280e326a6ac4c
voluntas <nakai@shiguredo.jp>
2025-08-01 16:03:05 +0900

    コード整理

--- packages/virtual-background/src/virtual_background.ts
+++ packages/virtual-background/src/virtual_background.ts
@@ -3,7 +3,7 @@ import {
   type SelfieSegmentationConfig,
   type Results as SelfieSegmentationResults,
 } from '@mediapipe/selfie_segmentation'
-import { VideoTrackProcessor } from '@shiguredo/video-track-processor'
+import { VideoTrackProcessor } from 'video-track-processor'
 import * as StackBlur from 'stackblur-canvas'
 
 /**
